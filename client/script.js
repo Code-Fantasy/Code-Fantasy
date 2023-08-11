d9c55272c51928f446deabccc8de77710a208e15
Maryam597 <maryam.dri@hotmail.fr>
2023-08-11 11:39:08 +0200

    Homepage ok

---
+++ client/script.js
@@ -0,0 +1,27 @@
+function showdiv(){
+  document.getElementById("image").style.visibility="visible";
+}
+
+setTimeout("showdiv()", 1000)
+
+
+
+
+
+
+function show(){
+  document.getElementById("startkey").style.visibility="visible";
+}
+
+setTimeout("show()", 3000)
+
+
+
+
+
+
+// function redirect()
+//     {
+//     var url = "http://www.(url).com";
+//     window.location(url);
+//     }
