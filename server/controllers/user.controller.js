58168013f6383356e70b32c2370a02c6210914f9
Theo0259 <theodewulf.pro@gmail.com>
2023-08-11 16:05:57 +0200

    controllers

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,119 @@
+const mysql = require("mysql");
+
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+conn.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to the database");
+});
+
+const createPlayer = (player, callback) => {
+  const { name } = player;
+  const query = "INSERT INTO Player (name) VALUES (?)";
+  conn.query(query, [name], (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, result.insertId);
+    }
+  });
+};
+
+const getPlayerById = (playerId, callback) => {
+  const query = "SELECT * FROM Player WHERE idPlayer = ?";
+  conn.query(query, [playerId], (err, rows) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, rows[0]);
+    }
+  });
+};
+
+const createJob = (job, callback) => {
+  const { stats, HP, attack, dodge, skills, slash, protera, strike, firaga, curaga, url } = job;
+  const query = "INSERT INTO Jobs (stats, HP, attack, dodge, skills, slash, protera, strike, firaga, curaga, url) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  conn.query(query, [stats, HP, attack, dodge, skills, slash, protera, strike, firaga, curaga, url], (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, result.insertId);
+    }
+  });
+};
+
+const createStage = (stage, callback) => {
+  const { url } = stage;
+  const query = "INSERT INTO Stage (url) VALUES (?)";
+  conn.query(query, [url], (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, result.insertId);
+    }
+  });
+};
+
+const createSound = (sound, callback) => {
+  const { battleTheme, victoryTheme, menuTheme } = sound;
+  const query = "INSERT INTO Sound (battleTheme, victoryTheme, menuTheme) VALUES (?, ?, ?)";
+  conn.query(query, [battleTheme, victoryTheme, menuTheme], (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, result.insertId);
+    }
+  });
+};
+
+const createXP = (xp, callback) => {
+  const { value } = xp;
+  const query = "INSERT INTO XP (value_XP) VALUES (?)";
+  conn.query(query, [value], (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, result.insertId);
+    }
+  });
+};
+
+const createTeam = (team, callback) => {
+  const { name } = team;
+  const query = "INSERT INTO Teams (name_Teams) VALUES (?)";
+  conn.query(query, [name], (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, result.insertId);
+    }
+  });
+};
+
+const createCrystal = (crystal, callback) => {
+  const { name, url } = crystal;
+  const query = "INSERT INTO Crystal (name_Crystal, url_Crystal) VALUES (?, ?)";
+  conn.query(query, [name, url], (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, result.insertId);
+    }
+  });
+};
+
+module.exports = {
+  createPlayer,
+  getPlayerById,
+  createJob,
+  createStage,
+  createSound,
+  createXP,
+  createTeam,
+  createCrystal,
+};
