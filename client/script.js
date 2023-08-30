0b52542f2b46f17660412e307dcd10b24b1b4789
Theo0259 <122873258+Theo0259@users.noreply.github.com>
2023-08-30 11:58:45 +0200

    test creation

--- client/script.js
+++ client/script.js
@@ -69,48 +69,106 @@ wmpic.addEventListener("click", () => {
   thiefdiv.style.display = "none";
 });
 
-// Sélectionnez la div du job "Warrior"
+// Déclarez une variable pour stocker l'ID du job sélectionné
+let selectedJobId = null;
+
+// Sélectionnez les div des personnages
 const warriorDiv = document.getElementById("warrior");
-// Ajoutez un gestionnaire d'événements au clic sur la div
+const thiefDiv = document.getElementById("thief");
+const monkDiv = document.getElementById("monk");
+const blackmageDiv = document.getElementById("blackmage");
+const whitemageDiv = document.getElementById("whitemage");
+
+// Ajoutez un gestionnaire d'événements au clic sur la div warrior
 warriorDiv.addEventListener("click", async () => {
   // Effectuez une requête à votre serveur pour obtenir les informations du job
   const response = await fetch(`http://localhost:8000/job/1`);
   const jobData = await response.json();
+  selectedJobId = jobData.idJobs_Jobs; // Attribuez l'ID du job sélectionné
   console.log(jobData);
 });
 
-const thiefDiv = document.getElementById("thief");
-// Ajoutez un gestionnaire d'événements au clic sur la div
+// Ajoutez un gestionnaire d'événements au clic sur la div thief
 thiefDiv.addEventListener("click", async () => {
   // Effectuez une requête à votre serveur pour obtenir les informations du job
   const response = await fetch(`http://localhost:8000/job/2`);
   const jobData = await response.json();
+  selectedJobId = jobData.idJobs_Jobs; // Attribuez l'ID du job sélectionné
   console.log(jobData);
 });
 
-const monkDiv = document.getElementById("monk");
-// Ajoutez un gestionnaire d'événements au clic sur la div
+// Ajoutez un gestionnaire d'événements au clic sur la div monk
 monkDiv.addEventListener("click", async () => {
   // Effectuez une requête à votre serveur pour obtenir les informations du job
   const response = await fetch(`http://localhost:8000/job/3`);
   const jobData = await response.json();
+  selectedJobId = jobData.idJobs_Jobs; // Attribuez l'ID du job sélectionné
   console.log(jobData);
 });
 
-const blackmageDiv = document.getElementById("blackmage");
-// Ajoutez un gestionnaire d'événements au clic sur la div
+// Ajoutez un gestionnaire d'événements au clic sur la div blackmage
 blackmageDiv.addEventListener("click", async () => {
   // Effectuez une requête à votre serveur pour obtenir les informations du job
   const response = await fetch(`http://localhost:8000/job/5`);
   const jobData = await response.json();
+  selectedJobId = jobData.idJobs_Jobs; // Attribuez l'ID du job sélectionné
   console.log(jobData);
 });
 
-const whitemageDiv = document.getElementById("whitemage");
-// Ajoutez un gestionnaire d'événements au clic sur la div
+// Ajoutez un gestionnaire d'événements au clic sur la div whitemage
 whitemageDiv.addEventListener("click", async () => {
   // Effectuez une requête à votre serveur pour obtenir les informations du job
   const response = await fetch(`http://localhost:8000/job/4`);
   const jobData = await response.json();
+  selectedJobId = jobData.idJobs_Jobs; // Attribuez l'ID du job sélectionné
   console.log(jobData);
 });
+
+function createPlayer() {
+  const playerName = document.getElementById("inputchara").value;
+
+  if (playerName.trim() === "") {
+    alert("Please enter a valid name.");
+    return;
+  }
+
+  const playerData = {
+    name_Player: playerName,
+    level_Player: 1,
+  };
+
+  fetch("/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(playerData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const playerId = data.playerId;
+      const playerJobData = {
+        idPlayer_Player: playerId,
+        idJobs_Jobs: selectedJobId, // Utilisez l'ID du job sélectionné
+      };
+
+      return fetch("/job/assign-to-player", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(playerJobData),
+      });
+    })
+    .then((response) => {
+      if (response.status === 201) {
+        alert("Player created successfully!");
+      } else {
+        alert("Error while creating player or assigning job.");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("An error occurred.");
+    });
+}
